35f8553acca304c5d9b02cf302e0621a40de4428
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-10-06 15:15:12 +0530

    style: add utility classes to index.css and use them in the homepage
    - style the link buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 export default function Home() {
   return (
     <div>
-      <section className="hero">
-        <h1 className="hero-heading">Conway's Game of Life</h1>
-        <p>
+      <section className="container flex-col center gap-2">
+        <h1 className="text-center">Conway's Game of Life</h1>
+        <p className="text-center">
           A cellular automaton devised by the British mathematician John Horton
           Conway in 1970.
         </p>
-      </section>
 
-      <section>
-        <Link to="/simulator">Simulator</Link>
-        <Link to="/about">About</Link>
+        <div className="flex gap-2 center">
+          <Link className="block link" to="/simulator">
+            Simulator
+          </Link>
+          <Link className="block link" to="/about">
+            About
+          </Link>
+        </div>
       </section>
     </div>
   );
