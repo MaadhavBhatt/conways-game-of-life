05b7d26c7bf595153e320e814c6fe8b7a8e818ea
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-10-06 18:28:01 +0530

    feat: add navigation link to Home in About and Simulator pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function About() {
   return (
     <section className="container">
       <h2>About this site</h2>
       <p>This site is a React implementation of Conway's Game of Life.</p>
+      <Link to="/">Go to Home</Link>
     </section>
   );
 }
