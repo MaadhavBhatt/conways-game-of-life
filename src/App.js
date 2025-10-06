89a2b26b6e62ab0084bb844321750360a55c0646
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-10-06 14:28:00 +0530

    feat: reorganize styles into src/styles folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Simulator from './pages/Simulator/Simulator';
